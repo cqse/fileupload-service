4be450032253c11747824a14d87494e9ee951dde
Fabian Streitel <streitel@cqse.eu>
2018-04-19 11:30:42 +0200

    use CliKt instead of ArgParser for better help messages

--- src/main/kotlin/eu/cqse/fileupload/Main.kt
+++ src/main/kotlin/eu/cqse/fileupload/Main.kt
@@ -1,26 +1,57 @@
 package eu.cqse.fileupload
 
-import com.xenomachina.argparser.ArgParser
-import com.xenomachina.argparser.mainBody
-import mu.KotlinLogging
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.*
+import com.github.ajalt.clikt.parameters.types.int
+import mu.KLogging
+import org.conqat.lib.commons.filesystem.FileSystemUtils
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.*
 
 private val programVersion = ResourceBundle.getBundle("eu.cqse.fileupload.app").getString("version")
-private val logger = KotlinLogging.logger {}
 
-/** Entry point */
-fun main(args: Array<String>) {
-    mainBody("eu.cqse.fileupload.jar") {
+/** Entry point. */
+class Main : CliktCommand() {
 
-        ArgParser(args).parseInto(::CommandLineArguments).run {
-            logger.info {
-                "Starting file upload receiver $programVersion on port $port. Writing output to $outputDirectory"
-            }
+    companion object : KLogging()
 
-            val commandRunner = CommandRunner(commandToRun)
-            UploadHandler(outputDirectory, commandRunner::runAsync).asServer(Jetty(port)).start()
+    val outputDirectory: Path by option(
+        "-o", "--out",
+        help = "Path under which the uploaded files will be stored."
+    )
+        .convert { Paths.get(it) }
+        .required()
+        .validate {
+            FileSystemUtils.ensureDirectoryExists(it.toFile())
         }
+
+    val port: Int by option(
+        "-p", "--port",
+        help = "Port under which to listen for uploads. Defaults to 9346."
+    )
+        .int()
+        .default(9346)
+
+    val commandToRun: String? by option(
+        "-c", "--command",
+        help = "Optional command to run after each upload. \$F will be replaced with the path to the uploaded file."
+    )
+
+    override fun run() {
+        logger.info {
+            "Starting file upload receiver $programVersion on port $port. Writing output to $outputDirectory"
+        }
+
+        val commandRunner = CommandRunner(commandToRun)
+        UploadHandler(outputDirectory, commandRunner::runAsync).asServer(Jetty(port)).start()
     }
+
+}
+
+/** Entry point. */
+fun main(args: Array<String>) {
+    Main().main(args)
 }
